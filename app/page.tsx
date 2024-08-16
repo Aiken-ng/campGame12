5b65a8215983b5d92c7a70116ccca30fdf44f3dc
Aiken-ng <147388974+Aiken-ng@users.noreply.github.com>
2024-08-16 23:30:34 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -32,42 +32,24 @@ export default function App() {
   }
 
   return (
-   <head>
-        <meta charset = "utf-8" />
-        <!-- Required meta tags -->
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" 
-        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous"/>
-        <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
-        <!--own css files-->
-        <link href="{{ url_for('static', filename='index.css') }}" rel="stylesheet"/>
-        <!--own javascript files-->
-        <script defer src="{{ url_for('static', filename='daynight.js') }}"></script>
-        <script defer src="{{ url_for('static', filename='lightbox.js') }}"></script>
-        <!--other HTML pages-->
-        <link href="pre-poly.html" rel="search"/>
-        <link href="working.html" rel="search"/>
-        <link href="number.html" rel="search"/>
-        <title>Home</title>
-    </head>
-
+   <>
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" />
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+      <link href="{{ url_for('static', filename='index.css') }}" rel="stylesheet" />
+      <script defer src="{{ url_for('static', filename='daynight.js') }}"></script>
+      <script defer src="{{ url_for('static', filename='lightbox.js') }}"></script>
+      <link href="pre-poly.html" rel="search" />
+      <link href="working.html" rel="search" />
+      <link href="number.html" rel="search" />
+      <title>Home</title>
+    </Head>
     <body>
-        <!--navigation bar-->
-        <header class="fixed-top">
-            <nav class="navbar navbar-expand-lg navbar-dark container">
-                <a href="/" class="navbar-brand"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi bi-house-door-fill" viewBox="0 0 16 16">
-                    <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z"/>
-                  </svg></a>
-            </nav>
-        </header>
-        <div class="herobanner">
-
-        </div>
-        <main>
-        </main>
+      {/* Rest of the component */}
     </body>
+  </>
   );
 }

74af1059715856549a112e043be0ca2d4bff80d8
Aiken-ng <147388974+Aiken-ng@users.noreply.github.com>
2024-08-16 23:35:40 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-import "./../app/all_pages.css";
+// import "./../app/all_pages.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 // import "@aws-amplify/ui-react/styles.css";
@@ -30,26 +30,6 @@ export default function App() {
       content: window.prompt("Todo content"),
     });
   }
-
   return (
-   <>
-    <Head>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" />
-      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-      <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
-      <link href="{{ url_for('static', filename='index.css') }}" rel="stylesheet" />
-      <script defer src="{{ url_for('static', filename='daynight.js') }}"></script>
-      <script defer src="{{ url_for('static', filename='lightbox.js') }}"></script>
-      <link href="pre-poly.html" rel="search" />
-      <link href="working.html" rel="search" />
-      <link href="number.html" rel="search" />
-      <title>Home</title>
-    </Head>
-    <body>
-      {/* Rest of the component */}
-    </body>
-  </>
   );
 }

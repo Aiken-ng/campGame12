f9789b921b12688fe258fa487ea0ef7f5f56ead0
Aiken-ng <147388974+Aiken-ng@users.noreply.github.com>
2024-08-17 01:15:09 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
-import styles from "./all_pages.css";
+// import styles from "./all_pages.css";
 import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import Link from "next/link"
@@ -15,41 +15,12 @@ const client = generateClient<Schema>();
 
 export default function App() {
   return (
-    <header>
-      <nav className={styles.navbar}>
-        <div className={styles.navbarBrand}>
-          <Link href="/">
-            <a>MyWebsite</a>
-          </Link>
-        </div>
-        <ul className={`${styles.navbarLinks} ${isOpen ? styles.active : ""}`}>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a>About</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a>Services</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <a>Contact</a>
-            </Link>
-          </li>
-        </ul>
-        <div className={styles.navbarToggle} onClick={toggleMenu}>
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
-      </nav>
-    </header>
+    <main>
+      <Link href="/"><a>Home</a></Link>
+      <Link href="/"><a>About Us</a></Link>
+      <Link href="/"><a>Contact Us</a></Link>
+      <Link href="/"><a>Interesting facts</a></Link>
+      <Link href="/"><a>Don't click me</a></Link>
+    </main>
   );
 }

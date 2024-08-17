5b0ba1442321ef24d68e583d3ba48db51bcae14d
Aiken-ng <147388974+Aiken-ng@users.noreply.github.com>
2024-08-17 22:32:19 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Navbar from "@/Components/Navbar";
 // import "./app.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,9 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div>
-        <Navbar />
-        <main>{children}</main>
-    </div>
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
   );
 }

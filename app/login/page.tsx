3039114458612a34198e9dec7ddd5838246eb64b
Aiken-ng <147388974+Aiken-ng@users.noreply.github.com>
2024-08-19 22:14:39 +0800

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,1 +1,18 @@
+import React from 'react';
+import '../App.css';
+import Amplify from 'aws-amplify'
+import awsconfig from 'aws-amplify';
+import {AmplifySignOut, withAuthenticator} from '@aws-amplify/ui-react';
 
+Amplify.configure(awsconfig);
+
+function App() {
+return{
+  <div className="App">
+    <header className='App-header'>
+      <AmplifySignOut/>
+      <h2>My App Content</h2>
+    </header>
+  </div>
+    };
+}

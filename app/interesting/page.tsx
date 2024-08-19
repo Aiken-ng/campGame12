b495a8c2f67a9faa44542796b1dc9e63d18215c6
Aiken-ng <147388974+Aiken-ng@users.noreply.github.com>
2024-08-19 16:18:00 +0800

    Update page.tsx

--- app/interesting/page.tsx
+++ app/interesting/page.tsx
@@ -1,28 +1,10 @@
-"use client";
-
-// import { useState, useEffect } from "react";
-// import { generateClient } from "aws-amplify/data";
-// import type { Schema } from "@/amplify/data/resource";
-// import "../all_pages.css";
-// import { Amplify } from "aws-amplify";
-// import outputs from "@/amplify_outputs.json";
-// import Link from "next/link"
 import type { AppProps } from 'next/app';
 import { Authenticator } from '@aws-amplify/ui-react';
 import { Amplify } from 'aws-amplify';
 import outputs from '@/amplify_outputs.json';
 import '@aws-amplify/ui-react/styles.css';
 
-// import "@aws-amplify/ui-react/styles.css";
-
 Amplify.configure(outputs);
-
-// <ul>
-//         <li><Link href="/"><a>Home</a></Link></li>
-//         <li><Link href="/about_us"><a>About Us</a></Link></li>
-//         <li><Link href="/interesting"><a>Interesting Facts</a></Link></li>
-//         <li><Link href="https://www.youtube.com/watch?v=xvFZjo5PgG0"><a>Do not click</a></Link></li>
-//         </ul>
 
 export default function App({ Component, pageProps }: AppProps) {
   return (

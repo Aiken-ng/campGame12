5927cc86a3486afc5f8fff8988a094a36903a1ea
Aiken-ng <147388974+Aiken-ng@users.noreply.github.com>
2024-08-19 18:17:40 +0800

    Update page.tsx

--- app/interesting/page.tsx
+++ app/interesting/page.tsx
@@ -1,21 +1,30 @@
-import type { AppProps } from 'next/app';
-import { Authenticator } from '@aws-amplify/ui-react';
-import { Amplify } from 'aws-amplify';
-import outputs from '@/amplify_outputs.json';
-import '@aws-amplify/ui-react/styles.css';
+"use client";
+
+import { useState, useEffect } from "react";
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import "../all_pages.css";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import Link from "next/link"
+// import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
-export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user?.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-          <Component {...pageProps} />
-        </main>
-      )}
-    </Authenticator>
+const client = generateClient<Schema>();
+
+export default function App() {
+  return(
+      <main>
+        <ul>
+        <li><Link href="/"><a>Home</a></Link></li>
+        <li><Link href="/about_us"><a>About Us</a></Link></li>
+        <li><Link href="/interesting"><a>Interesting Facts</a></Link></li>
+        <li><Link href="/login"><a>Do not click</a></Link></li>
+        </ul>
+      <div>
+        (In development...)
+      </div>
+        
   );
-};
+}

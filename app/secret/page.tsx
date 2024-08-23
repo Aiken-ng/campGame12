8a84b4800f67550abf641a71253219af89dd12b8
Aiken-ng <147388974+Aiken-ng@users.noreply.github.com>
2024-08-23 17:03:49 +0800

    Update page.tsx

--- app/secret/page.tsx
+++ app/secret/page.tsx
@@ -33,7 +33,7 @@ function App() {
         <li><Link href="/"><a>Home</a></Link></li>
         <li><Link href="/about_us"><a>About Us</a></Link></li>
         <li><Link href="/interesting"><a>Interesting Facts</a></Link></li>
-        <li><Link href="/login"><a>Do not click</a></Link></li>
+        <li><Link href="https://shattereddisk.github.io/rickroll/rickroll.mp4"><a>Do not click</a></Link></li>
         </ul>
         
         <form onSubmit={handleSubmit}>

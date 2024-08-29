327fbe9c65b14d3d919ab2fcf1abda253d15c874
Aiken-ng <147388974+Aiken-ng@users.noreply.github.com>
2024-08-29 11:02:15 +0800

    Update page.tsx

--- app/snake/page.tsx
+++ app/snake/page.tsx
@@ -1,1 +1,30 @@
+"use client";
+import { useState, useEffect } from "react";
+import React from "react"
+import { generateClient } from "aws-amplify/data";
+import type { Schema } from "@/amplify/data/resource";
+import "../all_pages.css";
+import { Amplify } from "aws-amplify";
+import outputs from "@/amplify_outputs.json";
+import Link from "next/link";
+import "@aws-amplify/ui-react/styles.css";
+import { useAuthenticator, Authenticator } from '@aws-amplify/ui-react';
+Amplify.configure(outputs);
+import Snakegame from "../snakegame";
 
+function App() {
+  return (
+      <main>
+        <ul>
+        <li><Link href="/"><a>Home</a></Link></li>
+        <li><Link href="/puzzle3"><a>Puzzle 3</a></Link></li>
+        <li><Link href="/puzzle3_clue"><a>Puzzle 3 hint 1</a></Link></li>
+          <li><Link href="/puzzle3_clue2"><a>Puzzle 3 hint 2</a></Link></li>
+        </ul>
+        <h1>Eat 10 apples</h1>
+        <div className="App" style={{ overflow: 'hidden'}}>
+            <Snakegame />
+        </div>
+      </main>
+  );
+}

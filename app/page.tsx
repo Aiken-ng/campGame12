3283b87c6ad09c4c9b8274640165eb8f489d83c2
Aiken-ng <147388974+Aiken-ng@users.noreply.github.com>
2024-08-29 11:50:23 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -63,15 +63,9 @@ export default function App() {
         <li><Link href="/puzzle3_clue2"><a>Puzzle 3 hint 2</a></Link></li>
         </ul>
       <div style={{ width: '300px' }}> {/* Set a container width if needed */}
-      <TypingEffect text="Weelcome to Puzzle 3!!!
-
-I am Agapiii, your guide!! I hope you had a good rest on the cushion!! Congratulations on coming this far!
- ∧,,,∧
-(  ̳• · • ̳)
-/    づ♡ 
-
+      <div>∧,,,∧<br />(  ̳• · • ̳)<br />/    づ♡</div>
+      <TypingEffect text="Weelcome to Puzzle 3!!! I am Agapiii, your guide!! I hope you had a good rest on the cushion!! Congratulations on coming this far!
 Here you'll find another puzzle! Solve it to get to advance in your quest! 
-
 Hint: Search the web page for lots of clues! Have fun!
 " speed={80} /></div>
       </main>

6327070a0ac37c31f7b7491f428c1cf4ccda48e2
Aiken-ng <147388974+Aiken-ng@users.noreply.github.com>
2024-08-29 10:56:58 +0800

    Update page.tsx

--- app/puzzle3_clue/page.tsx
+++ app/puzzle3_clue/page.tsx
@@ -13,9 +13,16 @@ Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 const handleLinkClick = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
-        event.preventDefault(); // Prevent the default link behavior
+        event.preventDefault();
         alert("Why did you click?");
-        alert("I told you not to click?");        
+        alert("I told you not to click?");
+        alert("Who asked you to click?");
+        alert("Didn't you read do not click?");
+        alert("But if you were wondering,");
+        alert('Is the word "click" a hyperlink?');
+        alert("Who knows what may happen if you try clicking it");
+        alert("Though let me warn you, it does say do not click");
+        alert("Or maybe clicking something is a good thing");
 };
 
 export default function about_us() {
@@ -28,7 +35,7 @@ export default function about_us() {
           <li><Link href="/puzzle3_clue2"><a>Puzzle 3 hint 2</a></Link></li>
         </ul>
       <div>
-        <a href="#" onClick={handleLinkClick}>Do not click</a>
+        <a href="#" style={{ textDecoration: 'underline' }} onClick={handleLinkClick}>Do not </a><a href="#" onClick={handleLinkClick}>Click</a>
       </div>
       </main>
   );
